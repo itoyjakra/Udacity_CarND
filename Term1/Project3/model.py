04ae4cb172b839b7004d8a035981200fb55bea4a
arko <arkajyoti@gmail.com>
2017-03-15 16:26:12 -0700

    first import of data_gen and train_model

--- Term1/Project3/model.py
+++ Term1/Project3/model.py
@@ -1,3 +1,32 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+import numpy as np
+import tensorflow as tf
+
+def data_gen(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
 def get_data():
     """
     capture image and steering data
@@ -12,8 +41,8 @@ def build_model(camera_format):
 
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5 - 1.,
-            input_shape=(ch, row, col),
-            output_shape=(ch, row, col)))
+                        input_shape=(ch, row, col),
+                        output_shape=(ch, row, col)))
     model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
     model.add(ELU())
     model.add(Convolution2D(32, 5, 5, subsample=(2, 2), border_mode="same"))
@@ -31,8 +60,21 @@ def build_model(camera_format):
 
     return model
 
+def train_model(model):
+    """
+    train model on training data
+    """
+    train_data_gen = data_gen(train_samples, batch_size=32)
+    validation_generator = data_gen(validation_samples, batch_size=32)
+
+    model.fit_generator(train_generator,
+                        samples_per_epoch=len(train_samples),
+                        validation_data=validation_generator,
+                        nb_val_samples=len(validation_samples),
+                        nb_epoch=3)
 
 if __name__ == "__main__":
     get_data()
     model = build_model((3, 160, 320))
+    train_model()
     predict_on_new_data()
