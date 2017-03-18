76ad25df263ec8241571a257596df8f1097e0b7b
itoyjakra <arkajyoti@gmail.com>
2017-03-18 04:09:33 -0500

    First trained comma.ai model     * build_model changed to model_comma_ai     * cropping included in model building stage (need keras 2.0.1)     * sklearn.cross_validation needed for train_test_split     * first version of comma_ai_model.h5

--- Term1/Project3/model.py
+++ Term1/Project3/model.py
@@ -8,12 +8,12 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import numpy as np
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 from sklearn.utils import shuffle
 
 def data_gen(samples, batch_size=32):
     num_samples = len(samples)
-    print ("num_samples = ", num_samples)
+    print ("number of samples  = ", num_samples)
     while 1: # Loop forever so the generator never terminates
         #shuffle(samples)
         for offset in range(0, num_samples, batch_size):
@@ -24,18 +24,12 @@ def data_gen(samples, batch_size=32):
             for batch_sample in batch_samples:
                 name = 'Udacity_Data/data/IMG/'+batch_sample[0].split('/')[-1]
                 center_image = cv2.imread(name)
-                #print (name, type(center_image))
-                # crop image here
-                row, col, ch = center_image.shape
-                center_image = center_image[50:row-20]
                 center_angle = float(batch_sample[3])
                 images.append(center_image)
                 angles.append(center_angle)
 
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
-            #print ("000", y_train)
             yield shuffle(X_train, y_train)
 
 
@@ -51,13 +45,13 @@ def get_data():
 
     return samples
 
-def build_model(camera_format):
+def model_comma_ai(camera_format):
     """
     create a regression deep network model
     """
     row, col, ch = camera_format # 3, 160, 320  # camera format
-    crop_top = 0 #50
-    crop_bot = 0 #20
+    crop_top = 50
+    crop_bot = 20
     crop_left = 0
     crop_right = 0
     row_cropped = row - crop_top - crop_bot
@@ -65,12 +59,10 @@ def build_model(camera_format):
 
 
     model = Sequential()
+    model.add(Cropping2D(cropping = ((crop_top, crop_bot), (crop_left, crop_right)), input_shape = (row, col, ch), data_format = "channels_last"))
     model.add(Lambda(lambda x: x/127.5 - 1.,
                         input_shape=(row_cropped, col_cropped, ch),
                         output_shape=(row_cropped, col_cropped, ch)))
-    #model.add(Cropping2D(cropping = ((crop_top, crop_bot), (crop_left, crop_right)),
-    #                     input_shape = (row, col, ch)))
-    #                    data_format = "channels_last")
 
     model.add(Convolution2D(16, 8, 8, subsample=(4, 4), border_mode="same"))
     model.add(ELU())
@@ -86,8 +78,6 @@ def build_model(camera_format):
     model.add(Dense(512))
     model.add(Dropout(1))
     model.add(ELU())
-
-    #model.add(Flatten())
     model.add(Dense(1))
 
     model.compile(optimizer="adam", loss="mse")
@@ -98,22 +88,21 @@ def train_model(model, data):
     """
     train model on supplied data
     """
-    #train_samples, validation_samples = train_test_split(data, test_size=0.2)
-    train_samples, validation_samples = train_test_split(data, test_size=0.0)
-    print ("size of samples: ", len(train_samples), len(validation_samples))
+    train_samples, validation_samples = train_test_split(data, test_size=0.2)
     train_generator = data_gen(train_samples, batch_size=32)
-    #validation_generator = data_gen(validation_samples, batch_size=32)
+    validation_generator = data_gen(validation_samples, batch_size=32)
 
     model.fit_generator(train_generator,
                         samples_per_epoch=len(train_samples),
-                        #validation_data=validation_generator,
-                        #nb_val_samples=len(validation_samples),
-                        nb_epoch=20)
+                        validation_data=validation_generator,
+                        nb_val_samples=len(validation_samples),
+                        nb_epoch=5)
 
     return model
 
 if __name__ == "__main__":
     data = get_data()
-    model = build_model((90, 320, 3))
+    model = model_comma_ai((160, 320, 3))
     train_model(model, data)
+    model.save('comma_ai_model.h5')
     # predict_on_new_data()
